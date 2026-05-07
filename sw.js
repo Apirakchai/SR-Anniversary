853261d012bbe1e6e1dbe754318a0718348f70fa
Apirakchai <apirakchai.s@gmail.com>
2026-05-08 02:18:02 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Safe & Ruang · Service Worker
    Caches the app shell for offline use.
 */
-const CACHE_NAME = 'sr-anniversary-v6';
+const CACHE_NAME = 'sr-anniversary-v7';
 const APP_SHELL = [
   './',
   './index.html',
